89fcd82f56702426a6a46ab219474c55c1a14740
James Jameson <james@raindesignlabs.net>
2026-05-09 01:52:25 -0700

    revert: restore real app after red screen diagnostic

--- app/src/main/java/tv/litebox/MainActivity.kt
+++ app/src/main/java/tv/litebox/MainActivity.kt
@@ -3,33 +3,21 @@ package tv.litebox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import tv.litebox.theme.ThemeManager
+import tv.litebox.ui.LiteBoxNavHost
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val themeManager = LiteBoxApp.instance.themeManager
+
         setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Red),
-                contentAlignment = Alignment.Center,
-            ) {
-                Text(
-                    text = "RENDERING OK",
-                    color = Color.White,
-                    style = MaterialTheme.typography.displayMedium,
-                )
-            }
+            val theme by themeManager.currentTheme.collectAsState()
+            LiteBoxNavHost(theme = theme)
         }
     }
 }
