9857ed97922953ceb3ee93a77fa1741d8d762b5c
James Jameson <james@raindesignlabs.net>
2026-05-09 01:38:05 -0700

    diag: red screen test for Compose rendering

--- app/src/main/java/tv/litebox/MainActivity.kt
+++ app/src/main/java/tv/litebox/MainActivity.kt
@@ -3,21 +3,33 @@ package tv.litebox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import tv.litebox.theme.ThemeManager
-import tv.litebox.ui.LiteBoxNavHost
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val themeManager = LiteBoxApp.instance.themeManager
-
         setContent {
-            val theme by themeManager.currentTheme.collectAsState()
-            LiteBoxNavHost(theme = theme)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Red),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(
+                    text = "RENDERING OK",
+                    color = Color.White,
+                    style = MaterialTheme.typography.displayMedium,
+                )
+            }
         }
     }
 }
